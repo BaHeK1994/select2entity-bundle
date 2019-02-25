9704792a08df28c2cbe4d3f624483d3b1be5eef9
tran <support@frallion.ru>
2019-02-25 16:46:20 +0300

    fix deprecation: "A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0."

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tetranz_select2_entity');
+        $treeBuilder = new TreeBuilder('tetranz_select2_entity');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
                 ->children()
